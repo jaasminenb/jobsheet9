d35fe23a1af11966b4a2cf3bd5532b3c4d4c5c33
jaasminenb <izzayaserere@gmail.com>
2025-11-13 08:55:14 +0700

    first commit

---
+++ ArrayBilangan12.java
@@ -0,0 +1,16 @@
+public class ArrayBilangan12 {
+public static void main(String[] args) {
+
+    int[] bil = new int[4];
+
+    bil[0] = 5;
+    bil[1] = 13;
+    bil[2] = -7;
+    bil[3] = 17;
+
+    for (int i = 0; i < bil.length; i++) {
+        System.out.println("bil[" + i + "] = " + bil[i]);
+    }
+}
+
+}
